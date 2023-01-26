2acd9bdabd8a80fa6fdf1f1a2a3c96964f5a3780
Alexandr Dylevich <dylevich220885@gmail.com>
2023-01-26 23:25:23 +0300

    created components Header, Navbar, Profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div className="app-wrapper">
+        <Header/>
+        <Navbar/>
+
+        <Profile/>
+      </div>
   );
 }
 

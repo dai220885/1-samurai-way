0e2cdb60a19afa43979c2ca8762c5e4e94015e57
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-19 17:56:07 +0300

    в App больше не передаем стор, а оборачиваем компонентой "StoreContext.Provider", в которую стор приходит через параметр value (video 44)

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ import store from './redux/redux-store';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
+import StoreContext from './StoreContext';
 
 
-let rerenderEntireTree = (state:StateType) => {
+
+let rerenderEntireTree = (state: StateType) => {
     //debugger
     ReactDOM.render(
         <BrowserRouter>
@@ -26,7 +28,10 @@ let rerenderEntireTree = (state:StateType) => {
             {/*    setNewPostText = {store.setNewPostText}*/}
             {/*    setNewMessageText={store.setNewMessageText}*/}
             {/*/>*/}
-            <App store={store} state = {state} dispatch={store.dispatch.bind(store)}/>
+            <StoreContext.Provider value={store}>
+                {/*<App store={store} state={state} dispatch={store.dispatch.bind(store)}/>*/}
+                <App />
+            </StoreContext.Provider>
         </BrowserRouter>,
         document.getElementById('root')
     );
@@ -35,14 +40,13 @@ let rerenderEntireTree = (state:StateType) => {
 rerenderEntireTree(store.getState());
 
 
-
 //далее функцию rerenderEntireTree передаем в качестве параметра в метод store.subscribe() (store.subscribe(rerenderEntireTree)) который  изменит поведение метода 'store._rerenderEntireTree' на поведение функции 'rerenderEntireTree', которая придет в качестве параметра
 
 //store.subscribe(rerenderEntireTree)
 //
 // было так, когда наш store сам передавал актуальный(измененный стейт) в функцию rerenderEntireTree (в методе dispatch), store из реакта так не делает, он просто уведомляет подписчика об изменении стейта, поэтому актуальный стейт нужно передать самостоятельно, для этого в subscribe передаем анонимную функцию, которая запустит наш rerenderEntireTree и передаст в нее актуальный стейт (store.getState())
 
-store.subscribe(()=>rerenderEntireTree(store.getState()))
+store.subscribe(() => rerenderEntireTree(store.getState()))
 
 
 /*

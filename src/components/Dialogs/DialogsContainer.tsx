718cdf72e4c69c936c256680da4608fe740ec7f3
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-22 22:36:38 +0300

    переписали контейнерные компоненты с помощью "mapStateToProps" "mapDispatchToProps" и "connect()"" (video 45-48)

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {
-    addMessageAC,
+    addMessageAC, MessagePageType,
     removeMessageAC,
     setNewMessageTextAC
 } from '../../redux/dialogs-reducer';
-import {ActionsType, StoreType} from '../../redux/store';
 import Dialogs from './Dialogs';
 import StoreContext from '../../StoreContext';
+import {connect} from 'react-redux';
+import MyPostsContainer from '../Profile/MyPosts/MyPostsContainer';
+import {AppStateType, DispatchType} from '../../redux/redux-store';
+import { Dispatch } from 'redux';
 
 export type DialogsContainerPropsType = {
     //store: StoreType
@@ -24,7 +27,7 @@ export type DialogsContainerPropsType = {
 //'DialogsContainer' в пропсах принимает весь 'store', после чего компоненту 'Dialogs' передает только нужные ей части 'store'
 
 //Контейнерные компоненты не получают стор в пропсах, а вызывают соответствующие презентационные компоненты, обернутые в <StoreContext.Consumer>, в которую приходит стор, после чего к нему и происходит обращение и передача нужных параметров в презентационную компоненту
-function DialogsContainer() {
+function DialogsContainerOLD() {
     return (
         <StoreContext.Consumer>
             {(store) => {
@@ -42,12 +45,28 @@ function DialogsContainer() {
                     removeMessage={removeMessage}
                     setNewMessageText={setNewMessageText}
                 />}}
-
-
         </StoreContext.Consumer>
-
     )
 }
+
+let mapStateToProps = (state: AppStateType): MessagePageType =>{
+    return {
+        dialogs: state.messagesPage.dialogs,
+        messages: state.messagesPage.messages,
+        newMessageText: state.messagesPage.newMessageText
+    }
+}
+//типизировали dispatch импортировав { Dispatch } from 'redux', DispatchType из 'redux-store' тоже работает;
+let mapDispatchToProps = (dispatch: Dispatch) =>{
+    return {
+        addNewMessage: ()=>{dispatch(addMessageAC())},
+        removeMessage: (messageForRemoveID: string)=>{dispatch(removeMessageAC(messageForRemoveID))},
+        setNewMessageText: (newPostText: string) => {dispatch(setNewMessageTextAC(newPostText))}
+    }
+}
+
+//контейнерная компонента с использованием react-redux:
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs); //когда две пары круглых скобок, то значит, что после первого вызова функция что-то вернет, а вторыми скобками мы вызываем, то, что вернется после первого вызова)))
 
 export default DialogsContainer;
 
--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import {
-    addPostAC,
+    addPostAC, ProfilePageType,
     removePostAC,
     setNewPostTextAC
 } from '../../../redux/profile-reducer';
-import {ActionsType, StoreType} from '../../../redux/store';
 import MyPosts from './MyPosts';
 import StoreContext from '../../../StoreContext';
+import {connect} from 'react-redux';
+import {addMessageAC, removeMessageAC, setNewMessageTextAC} from '../../../redux/dialogs-reducer';
+import Dialogs from '../../Dialogs/Dialogs';
+import {AppStateType, DispatchType} from '../../../redux/redux-store';
+import { Dispatch } from 'redux';
 
 type MyPostsContainerPropsType = {
     //store: StoreType
@@ -15,9 +19,7 @@ type MyPostsContainerPropsType = {
 //'MyPostsContainer' в пропсах принимает весь 'store', после чего компоненту 'MyPosts' передает только нужные ей части 'store'
 
 //Контейнерные компоненты не получают стор в пропсах, а вызывают соответствующие презентационные компоненты, обернутые в <StoreContext.Consumer>, в которую приходит стор, после чего к нему и происходит обращение и передача нужных параметров в презентационную компоненту
-function MyPostsContainer() {
-
-
+function MyPostsContainerOLD() {
     return (
         <StoreContext.Consumer>
             {(store) => {
@@ -41,11 +43,28 @@ function MyPostsContainer() {
                         setNewPostText={setNewPostText}
                     />
                 }
-
             }
-
         </StoreContext.Consumer>
     )
 }
+
+
+let mapStateToProps = (state: AppStateType): ProfilePageType =>{
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+    }
+}
+//типизировали dispatch импортировав { Dispatch } from 'redux', DispatchType из 'redux-store' тоже работает;
+let mapDispatchToProps = (dispatch: Dispatch) =>{
+    return {
+        addNewPost: () => dispatch(addPostAC()),
+        removePost: (postForRemoveID: string) => dispatch(removePostAC(postForRemoveID)),
+        setNewPostText: (newPostText: string) => dispatch(setNewPostTextAC(newPostText))
+    }
+}
+
+//контейнерная компонента с использованием react-redux:
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts); //когда две пары круглых скобок, то значит, что после первого вызова функция что-то вернет, а вторыми скобками мы вызываем, то, что вернется после первого вызова)))
 
 export default MyPostsContainer;

56c02cacd5d45c722ae209663fd32b8fa180b335
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-26 11:38:15 +0300

    в combineReducers()  дополнительно передал usersReducer (video 49)

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,16 +3,18 @@ import {combineReducers, legacy_createStore as createStore} from "redux"; //дл
 import dialogsReducer from './dialogs-reducer';
 import profileReducer from './profile-reducer';
 import sidebarReducer from './sidebar-reducer';
+import usersReducer from './users-reducer';
 
 //в функции createStore() происходит создание стейта. createStore() в качестве параметра принимает combineReducers() со всеми  рудьюсерами
 
 let rootReducer = combineReducers({
     messagesPage: dialogsReducer,
     profilePage: profileReducer,
-    sidebar: sidebarReducer
-}); //воспринимаем объект не как связку редьсеров, а как стейт
+    sidebar: sidebarReducer,
+    userPage: usersReducer
+}); //воспринимаем объект не как связку редьсеров, а как стейт со свойствами messagesPage, profilePage и т.д., в которых находятся объекты, возвращаемые соответствующими редьюсерами
 //
-// в combineReducers передали объект вида: {profileReducer: profileReducer,dialogsReducer: dialogsReducer,sidebarReducer: sidebarReducer} (когда в объекте название свойства и его значение идентичны, можно писать более коротко, просто название свойства (будет подразумеваться, что значение будет такое же)
+// в combineReducers передали объект вида: {profileReducer: profileReducer,dialogsReducer: dialogsReducer,sidebarReducer: sidebarReducer,  userPage: usersReducer} (когда в объекте название свойства и его значение идентичны, можно писать более коротко, просто название свойства (будет подразумеваться, что значение будет такое же)
 
 export type AppStateType = ReturnType<typeof rootReducer>
 export type DispatchType = typeof store.dispatch //(action: ActionsType) => void

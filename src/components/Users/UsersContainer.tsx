698f4dc045447c1fa9c5620b4ea63a13e874b017
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-26 11:34:04 +0300

    создал контейнерную компоненту  UsersContainer (video 49)

---
+++ src/components/Users/UsersContainer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Users} from './Users';
+import {AppStateType} from '../../redux/redux-store';
+import {followUserAC, setUsersAC, unFollowUserAC, UserPageType, UserType} from '../../redux/users-reducer';
+import {Dispatch} from 'redux';
+
+
+let mapStateToProps = (state: AppStateType): UserPageType => {
+    return {
+        users: state.userPage.users
+    }
+}
+
+let mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        followUser: (userId: string) => dispatch(followUserAC(userId)),
+        unfollowUser: (userId: string) => dispatch(unFollowUserAC(userId)),
+        setUsers: (users: UserType[]) => dispatch(setUsersAC(users)),
+    }
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users); //когда две пары круглых скобок, то значит, что после первого вызова функция что-то вернет, а вторыми скобками мы вызываем, то, что вернется после первого вызова)))
+
+export default UsersContainer;

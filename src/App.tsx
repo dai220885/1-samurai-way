b7e44e9ee7987f4f6e7817fb5940d3aa6499307b
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-28 22:59:27 +0300

    обновил библиотеку gh-pages (video 51)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {BrowserRouter, Route} from 'react-router-dom';
+//import { Routes, Route, Navigate } from 'react-router-dom'
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 

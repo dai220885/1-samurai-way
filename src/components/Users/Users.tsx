d88a0521d088079b9a070a69e01201e5a1c4ae02
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-28 22:07:37 +0300

    В компоненту Users добавили запрос на сервер (при условии, что список юзеров пуст), в котором получаем список юзеров, мапим их для соответствия нашему типу юзеров и отображаем (video 51)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {UserType} from '../../redux/users-reducer';
 import styles from './Users.module.css'
 import {v1} from 'uuid';
+import axios from 'axios';
+//import * as axios from 'axios'; //? в видео так сказано делать
 
 export type UsersPropsType = {
     users: UserType[]
@@ -10,37 +12,54 @@ export type UsersPropsType = {
     setUsers: (users: UserType[]) => void
 }
 export const Users = (props: UsersPropsType) => {
-    if (props.users.length === 3) {props.setUsers(
-        [
-            {
-                id: v1(),
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
-                followed: true,
-                fullName: 'Alexandr2',
-                status: 'i am a student',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: v1(),
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
-                followed: false,
-                fullName: 'Victor2',
-                status: 'i like music',
-                location: {city: 'Vitebsk', country: 'Belarus'}
-            },
-            {
-                id: v1(),
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
-                followed: true,
-                fullName: 'Sergey2',
-                status: 'good boy',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-        ],
-    )}
+    if (props.users.length === 0){
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response =>{
+                //debugger
+                //в newUsers положили массив новых пользователей, которых получили, промапив массив response.data.items и преобразовав в тип UserType (el в мапе ругался на типизацию (Parameter 'el' implicitly has an 'any' type), пришлось указать его тип). затем полученный массив newUsers передали в колбэк из пропсов (props.setUsers())
+                //можно было просто изменить UserType под тот формат данных о пользователе, который приходит с сервера
+                let newUsers: UserType[] = response.data.items.map((el:{ id: any; photos: {small: string, large: string}; followed: any; name: any; status: any; }):UserType =>
+                    ({
+                        id: el.id,
+                        photoUrl: el.photos.small,
+                        followed: el.followed,
+                        fullName: el.name,
+                        status: el.status,  location: {
+                            city: '',
+                            country: ''
+                        }}))
+                props.setUsers(newUsers)
+            })
+    }
 
-
-
+    // {props.setUsers(
+    //     [
+    //         {
+    //             id: v1(),
+    //             photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
+    //             followed: true,
+    //             fullName: 'Alexandr2',
+    //             status: 'i am a student',
+    //             location: {city: 'Minsk', country: 'Belarus'}
+    //         },
+    //         {
+    //             id: v1(),
+    //             photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
+    //             followed: false,
+    //             fullName: 'Victor2',
+    //             status: 'i like music',
+    //             location: {city: 'Vitebsk', country: 'Belarus'}
+    //         },
+    //         {
+    //             id: v1(),
+    //             photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiQSa00Huhj-j07rhDP24F3O0X0i05SQwI4A&usqp=CAU',
+    //             followed: true,
+    //             fullName: 'Sergey2',
+    //             status: 'good boy',
+    //             location: {city: 'Moscow', country: 'Russia'}
+    //         },
+    //     ],
+    // )}
 
     //в зависимости от того, подписаны ли мы на пользователя, в коллбэк кнопки передается либо функция followUser, либо unFollowUser
     let buttonOnClickHandler = (followed: boolean, userId: string) => {

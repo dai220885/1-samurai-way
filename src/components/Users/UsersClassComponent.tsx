160b12a5c79bc5a5e6d4549ec4d9b3362f0b9645
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-03 12:44:56 +0300

    описали классовую компоненту UsersClassComponent' и используем ее в качестве презентационной вместо 'Users' (при передаче в 'UsersContainer') (video 54)

---
+++ src/components/Users/UsersClassComponent.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {UserType} from '../../redux/users-reducer';
+import styles from './Users.module.css'
+import {v1} from 'uuid';
+import axios from 'axios';
+import {UsersPropsType} from './Users';
+//import * as axios from 'axios'; //? в видео так сказано делать
+
+
+export class UsersClassComponent extends React.Component <UsersPropsType>{
+    // constructor(props:UsersPropsType) {
+    //     super(props);
+    // } //если конструктор ничего не делает, кроме передачи пропсов конструктору родительского класса, то его можно не писать, т.к. все это и так происходит по умолчанию
+    componentDidUpdate(prevProps: Readonly<UsersPropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        console.log('componentDidUpdate')
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response =>{
+                //debugger
+                //в newUsers положили массив новых пользователей, которых получили, промапив массив response.data.items и преобразовав в тип UserType (el в мапе ругался на типизацию (Parameter 'el' implicitly has an 'any' type), пришлось указать его тип). затем полученный массив newUsers передали в колбэк из пропсов (props.setUsers())
+                //можно было просто изменить UserType под тот формат данных о пользователе, который приходит с сервера
+                const photoAvatarUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXTBU5APFtvEEsvOwobgk6YEjQUVuQjZTbGFKRjDNQ1iQwK0mTbpHoUZFPAgEFvlaf8gY&usqp=CAU'
+                let newUsers: UserType[] = response.data.items.map((el:{ id: any; photos: {small: string, large: string}; followed: any; name: any; status: any; }):UserType =>
+                    ({
+                        id: el.id,
+                        photoUrl: el.photos.small? el.photos.small: photoAvatarUrl,
+                        followed: el.followed,
+                        fullName: el.name,
+                        status: el.status,  location: {
+                            city: '',
+                            country: ''
+                        }}))
+                this.props.setUsers(newUsers)
+            })
+    }
+
+    followOnClickHandler = (followed: boolean, userId: string) => {
+        followed ? this.props.unfollowUser(userId) : this.props.followUser(userId)
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(user =>
+                        <div key={user.id}>
+                        <span>
+                            <div>
+                                <img src={user.photoUrl} alt = '   photo   'className={styles.userPhoto}/>
+                            </div>
+                            <div>
+                                <button onClick={() => {
+                                    this.followOnClickHandler(user.followed, user.id)
+                                }}>
+                                    {user.followed ? 'Unfollow' : 'Follow'} </button>
+                            </div>
+                        </span>
+                            <span>
+                            <span>
+                                <div>{`${user.fullName} - ${user.followed}`}</div><div>{user.status}</div>
+                            </span>
+                            <span>
+                                <div>{user.location.country}</div>
+                                <div>{user.location.city}</div>
+                            </span>
+                        </span>
+                        </div>)
+                }
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+

689d7787e7f502cee85059a68e73c1155d39132e
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-03 12:43:24 +0300

    используем 'UsersClassComponent' вместо 'Users' для передачи в контейнерную компоненту 'UsersContainer' (video 54)

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -4,6 +4,7 @@ import {Users} from './Users';
 import {AppStateType} from '../../redux/redux-store';
 import {followUserAC, setUsersAC, unFollowUserAC, UserPageType, UserType} from '../../redux/users-reducer';
 import {Dispatch} from 'redux';
+import {UsersClassComponent} from './UsersClassComponent';
 
 
 let mapStateToProps = (state: AppStateType): UserPageType => {
@@ -20,6 +21,7 @@ let mapDispatchToProps = (dispatch: Dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users); //когда две пары круглых скобок, то значит, что после первого вызова функция что-то вернет, а вторыми скобками мы вызываем, то, что вернется после первого вызова)))
+//const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users); //когда две пары круглых скобок, то значит, что после первого вызова функция что-то вернет, а вторыми скобками мы вызываем, то, что вернется после первого вызова)))
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersClassComponent);
 
 export default UsersContainer;

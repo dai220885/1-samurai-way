6fb2e72ffa37788dbbf092fe5f682da528a46eb9
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-08 19:32:19 +0300

    Profile в пропсах теперь принимает профиль пользователя и прокидывает его в ProfileInfo для отображения (video 59)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,11 +3,16 @@ import classes from './Profile.module.css'
 import mainLogo from "./../../images/main.png"
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from './MyPosts/MyPostsContainer';
+import {UserProfileType} from '../../redux/profile-reducer';
 
-function Profile () {
+type ProfilePropsType = {
+    profile: UserProfileType | undefined
+}
+
+function Profile (props: ProfilePropsType) {
     return (
         <div>
-            <ProfileInfo/>
+            <ProfileInfo profile={props.profile}/>
             {/*<MyPosts*/}
             {/*    posts={props.posts}*/}
             {/*    newPostText = {props.newPostText}*/}

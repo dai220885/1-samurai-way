bcf40d022514684264736072fb554343b64516c9
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-08 19:33:45 +0300

    в initialState добавлено свойство profile (добавлена его типизация) в котором будет информация о профиле пользователя, которую будем запрашивать с сервера (video 59)

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -3,15 +3,38 @@ import {v1} from 'uuid';
 const ADD_POST = 'ADD-POST';
 const REMOVE_POST = 'REMOVE-POST';
 const SET_NEW_POST_TEXT = 'SET-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET-USER-PROFILE';
 
 export type PostType = {
     id: string
     message: string
     likeCount: number
 }
+export type UserProfileType = {
+    aboutMe: string,
+    contacts: {
+        facebook: string,
+        website: string,
+        vk: string,
+        twitter: string,
+        instagram: string,
+        youtube: string,
+        github: string,
+        mainLink: string
+    },
+    lookingForAJob: boolean,
+    lookingForAJobDescription: string,
+    fullName: string,
+    userId: number,
+    photos: {
+        small: string,
+        large: string
+    }
+}
 export type ProfilePageType = {
     posts: PostType[]
     newPostText: string
+    profile: UserProfileType | undefined
 }
 let initialState: ProfilePageType = {
     posts: [
@@ -20,7 +43,8 @@ let initialState: ProfilePageType = {
         {id: v1(), message: 'By-By', likeCount: 4},
         {id: v1(), message: 'How are you?', likeCount: 1},
     ],
-    newPostText: ''
+    newPostText: '',
+    profile: undefined
 }
 
 const profileReducer = (state: ProfilePageType = initialState, action: ProfileReducerActionType): ProfilePageType => {
@@ -45,6 +69,9 @@ const profileReducer = (state: ProfilePageType = initialState, action: ProfileRe
             return {...state, newPostText: action.payload.newPostText};
             //state.newPostText = action.payload.newPostText;
         }
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.payload.profile}
+        }
         default: return state;
     }
 }
@@ -53,14 +80,17 @@ export type ProfileReducerActionType =
     AddPostActionType
     | RemovePostActionType
     | SetNewPostTextActionType
+    |SetUserProfileActionType
 
 //автоматически типизируем ActionCreator-ы, но в ActionCreator-е обязательно добавлять в конце 'as const', чтобы свойство type воспринималось не как любая строка, а как константа:
-export type AddPostActionType =  ReturnType<typeof addPostAC>//можно делать так, чтобы не дублировать
-export type RemovePostActionType = ReturnType<typeof removePostAC> //можно делать так, чтобы не дублировать
-export type SetNewPostTextActionType = ReturnType<typeof setNewPostTextAC>//можно делать так, чтобы не дублировать
+export type AddPostActionType =  ReturnType<typeof addPost>//можно делать так, чтобы не дублировать
+export type RemovePostActionType = ReturnType<typeof removePost> //можно делать так, чтобы не дублировать
+export type SetNewPostTextActionType = ReturnType<typeof setNewPostText>//можно делать так, чтобы не дублировать
+export type SetUserProfileActionType = ReturnType<typeof setUserProfile>//м
 
 //функции (ActionCreator-ы), которые будут создавать объекты action
-export const addPostAC =()=> ({type: ADD_POST}) as const
-export const removePostAC = (postForRemoveId: string) => ({type: REMOVE_POST, payload: {postForRemoveId}}) as const
-export const setNewPostTextAC = (newPostText:string) => ({type: SET_NEW_POST_TEXT, payload: {newPostText}}) as const
+export const addPost =()=> ({type: ADD_POST}) as const
+export const removePost = (postForRemoveId: string) => ({type: REMOVE_POST, payload: {postForRemoveId}}) as const
+export const setNewPostText = (newPostText:string) => ({type: SET_NEW_POST_TEXT, payload: {newPostText}}) as const
+export const setUserProfile = (profile: any) => ({type: SET_USER_PROFILE, payload: {profile}}) as const //fix any type !!!!!!!!!!!!!!!!!!!!!!!!
 export default profileReducer;

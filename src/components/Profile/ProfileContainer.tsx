9d9bb712cfa80d9dc61f349689abe065736a433e
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-13 17:33:25 +0300

    в componentDidUpdate делаем запрос на сервер если match.params.userId и profile?.userId не равны (айдишка пользователя в пропсах не отличается от той, которая в адресной строке (video 61)

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -12,18 +12,31 @@ import {setUserProfile, UserProfileType} from '../../redux/profile-reducer';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 
 class ProfileClassComponent extends React.Component <RootProfilePropsType> {
+
     // constructor(props:RootProfilePropsType) {
     //     super(props);
     // }
     componentDidUpdate(prevProps: Readonly<RootProfilePropsType>, prevState: Readonly<{}>, snapshot?: any) {
         console.log('ProfileClassComponent.componentDidUpdate')
+        //console.log(this.props.match.params.userId)
+        //console.log(this.props.profile)
+
+        //запрос на сервер выполнится только в том случае, если match.params.userId и profile?.userId будут разные, такое происходит, когда на страничке юзеров кликнули на аватарку, перешли на страничку профиля (с инфой о кликнутом юзере) и при этом кликаем на собственный логин(верхний правый угол), срабатывает componentDidUpdate, в роутинге меняется адрес на profile/наш_айдишник, но в пропсах еще предыдущий юзер сидит со своей айдишкой
+        if (String(this.props.match.params.userId) !== String(this.props.profile?.userId)) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${this.props.match.params.userId}`)
+                .then(response => {
+                    this.props.setUserProfile(response.data)
+                })
+        }
+        //this.render()
+
     }
 
     componentDidMount() {
         //debugger
         console.log('ProfileClassComponent.componentDidMount')
-        let userId = this.props.match.params.userId||'2'
-        console.log(`userId: ${userId}`)
+        let userId = this.props.match.params.userId||'28386'
+        //console.log(`userId: ${userId}`)
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(response => {
                 //debugger
@@ -32,6 +45,7 @@ class ProfileClassComponent extends React.Component <RootProfilePropsType> {
     }
 
     render() {
+        //debugger
         return (
             <Profile profile={this.props.profile}/>
         )

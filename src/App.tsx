63aaa0f80b842e58279dc580ede05f77af07c3bc
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-13 17:38:20 +0300

    отрисовываем <HeaderContainer/>  вместо простого <Header/> (video 61)

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App: React.FC = () => {
     let [count, setCount] = useState<number>(1)
@@ -28,7 +29,7 @@ const App: React.FC = () => {
                 }}>Add count
                 </button>
             </div>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
                 <Route

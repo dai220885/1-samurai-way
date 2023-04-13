c1a0b5024c2621f317afc564458197d465bdd257
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-13 17:36:57 +0300

    В хэдере в правом верхнем углу отображаем либо ссылку на залогинивание))) либо нашу айдишку (если уже залогинены) (video 61)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 import logo from '../../logo.svg';
 import classes from './Header.module.css'
+import {NavLink} from 'react-router-dom';
+import {AuthUserDataType} from '../../redux/auth-reducer';
 
-function Header () {
-    return(
+function Header(props: AuthUserDataType) {
+    return (
         <header className={classes.header}>
             <div></div>
-            <img src={logo} alt = 'logo'></img>
+            <img src={logo} alt="logo"></img>
+            <div className={classes.loginBlock}>
+                {props.isAuth
+                    ? <NavLink to={'/profile/'+ props.id}>{props.login}</NavLink>
+                    : <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     )
 }

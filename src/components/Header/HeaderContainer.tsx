46f3e323e6e34f843e116bbb2cbbc2a967fa98df
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-13 17:34:41 +0300

    в componentDidUpdate делаем запрос на сервер если match.params.userId и profile?.userId не равны (айдишка пользователя в пропсах не отличается от той, которая в адресной строке (video 61)

---
+++ src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Header from './Header';
+import {AuthReducerActionType, AuthUserDataType, setAuthUserDataAC} from '../../redux/auth-reducer';
+import {AppStateType} from '../../redux/redux-store';
+import {Dispatch} from 'redux';
+import {connect} from 'react-redux';
+import axios from 'axios';
+
+class HeaderClassComponent extends React.Component <RootHeaderPropsType> {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
+            .then(response => {
+                //debugger
+                if (response.data.resultCode === 0) {
+                    this.props.setAuthUserData(response.data.data)
+                }
+            })
+    }
+
+    render() {
+        return (
+            <Header {...this.props}/>
+        )
+    }
+}
+
+export type RootHeaderPropsType = MapStateToPropsType & MapDispatchToPropsType
+
+type MapStateToPropsType = ReturnType<typeof mapStateToProps>
+let mapStateToProps = (state: AppStateType) => {
+    return {
+        id: state.auth.id,
+        email: state.auth.email,
+        login: state.auth.login,
+        isAuth: state.auth.isAuth,
+        //isFetching: state.auth.isFetching,
+    }
+}
+
+type MapDispatchToPropsType = ReturnType<typeof mapDispatchToProps>
+let mapDispatchToProps = (dispatch: Dispatch<AuthReducerActionType>) => {
+    return {
+        setAuthUserData: (authUserData: AuthUserDataType) => {
+            dispatch(setAuthUserDataAC(authUserData))
+        },
+    }
+}
+
+const HeaderContainer = connect(mapStateToProps, mapDispatchToProps)(HeaderClassComponent);
+
+
+export default HeaderContainer;

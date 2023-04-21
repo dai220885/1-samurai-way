ef50b3effb5fc0c970a3178bf56a753d1a363f84
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-21 14:22:30 +0300

    убрал закомментированный участок кода (video 68)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,29 +1,17 @@
-import React, {useState} from "react";
-import classes from './Profile.module.css'
-import mainLogo from "./../../images/main.png"
+import React from "react";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import {UserProfileType} from '../../redux/profile-reducer';
 
 type ProfilePropsType = {
-    profile: UserProfileType | undefined
+    profile: UserProfileType | undefined,
 }
 
 function Profile (props: ProfilePropsType) {
     return (
         <div>
             <ProfileInfo profile={props.profile}/>
-            {/*<MyPosts*/}
-            {/*    posts={props.posts}*/}
-            {/*    newPostText = {props.newPostText}*/}
-            {/*    dispatch={props.dispatch}*/}
-            {/*    //addNewPost={props.addNewPost}*/}
-            {/*    //removePost={props.removePost}*/}
-            {/*    //setNewPostText={props.setNewPostText}*/}
-            {/*/>*/}
-            <MyPostsContainer
-                // store={props.store}
-            />
+            <MyPostsContainer/>
         </div>
     )
 }

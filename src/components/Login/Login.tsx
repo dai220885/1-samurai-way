340615b9390a5f73df860f70c50779a0f9c7dce6
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-21 14:16:53 +0300

    добавил  компонент <Login/>. Пока это просто заглушка с заголовком (video 68)

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type LoginPropsType = {
+
+}
+
+export const Login = (props: LoginPropsType) => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+};

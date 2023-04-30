8b0b1f9b5cd34c5e1bb340264b86dbb8a92bffbd
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-01 00:46:28 +0300

    в компоненту ProfileStatus добавлен локальный стейт и методы, изменяющие его значение
    (video 71)

--- src/components/Profile/ProfileInfo/ProfileIstatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileIstatus.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import classes from './ProfileInfo.module.css'
-import mainLogo from "./../../../images/main.png"
+import mainLogo from './../../../images/main.png'
 import {UserProfileType} from '../../../redux/profile-reducer';
 import {Preloader} from '../../common/Preloader/Preloader';
 //import styles from '../../Users/Users.module.css';
@@ -10,16 +10,40 @@ type ProfileStatusPropsType = {
     //profile: UserProfileType | undefined
 }
 
-class ProfileStatus extends React.Component <ProfileStatusPropsType>{
+class ProfileStatus extends React.Component <ProfileStatusPropsType> {
+    state = {
+        editMode: false
+    }
+    //используем стрелочную функцию, чтобы не нужно было использовать bind при передаче метода в качестве колбэка
+    activateEditMode = () => {
+        //debugger
+        this.setState(
+            {editMode: true}
+        )
+    }
+    deactivateEditMode = () => {
+        //debugger
+        this.setState(
+            {editMode: false}
+        )
+    }
     render() {
         return (
             <>
-                <div>
-                    <span>{this.props.status}</span>
-                </div>
-                <div>
-                    <input value={this.props.status}/>
-                </div>
+                {!this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input
+                            value={this.props.status}
+                            onBlur={this.deactivateEditMode}
+                            autoFocus={true}/>
+                    </div>
+                }
+
             </>
         )
     }

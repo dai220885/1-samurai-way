1ee5baf4c697d8fde9649ea08d155e52a315bf16
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-30 21:28:13 +0300

    добавлена отрисовка компоненты ProfileStatus
    (video 71)

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import classes from './ProfileInfo.module.css'
 import mainLogo from "./../../../images/main.png"
 import {UserProfileType} from '../../../redux/profile-reducer';
 import {Preloader} from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileIstatus';
 //import styles from '../../Users/Users.module.css';
 
 type ProfileInfoPropsType = {
@@ -15,12 +16,13 @@ function ProfileInfo(props: ProfileInfoPropsType) {
     } else {
         return (
             <div>
-                <div>
-                    {/*<img src="https://i1.mybook.io/p/x480/bookset/0e/4e/0e4e921f-8dd7-4ba2-834d-7b1161992f83.png" alt='main logo'></img>*/}
-                    <img className={classes.mainLogo} src={mainLogo} alt='main logo'></img>
-                </div>
+                {/*<div>*/}
+                {/*    /!*<img src="https://i1.mybook.io/p/x480/bookset/0e/4e/0e4e921f-8dd7-4ba2-834d-7b1161992f83.png" alt='main logo'></img>*!/*/}
+                {/*    <img className={classes.mainLogo} src={mainLogo} alt='main logo'></img>*/}
+                {/*</div>*/}
                 <div className={classes.descriptionBlock}>
                     {/*avatar + describtion*/}
+
                     <div>
                         {props.profile.aboutMe}
                     </div>
@@ -28,6 +30,7 @@ function ProfileInfo(props: ProfileInfoPropsType) {
                         {  `userID: ${props.profile.userId}`}
                     </div>
                     <img src={props.profile.photos.large} alt="   photo   " className={classes.profilePhoto}/>
+                    <ProfileStatus status={'some status'}/>
                 </div>
 
 

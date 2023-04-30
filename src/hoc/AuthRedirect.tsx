71c9a0d438a0d72fe03539404492c40f5fdd8ae9
Alexandr Dylevich <dylevich220885@gmail.com>
2023-04-30 20:58:00 +0300

    Добавлена типизация withAuthRedirect (video 70)

--- src/hoc/AuthRedirect.tsx
+++ src/hoc/AuthRedirect.tsx
@@ -3,20 +3,21 @@ import {Redirect} from 'react-router-dom';
 import {AppStateType} from '../redux/redux-store';
 import {connect} from 'react-redux';
 
-let mapStateToPropsForRedirect = (state: AppStateType) => ({
-    isAuth: state.auth.isAuth,
-})
-
-export const withAuthRedirect = (Component: any) => {
-    class RedirectComponent extends React.Component<any, any> {
-        render() {
-            if (!this.props.isAuth) return <Redirect to='/login'/>
-            return <Component {...this.props}/>
-        }
+type MapStateToPropsType = ReturnType<typeof mapStateToProps>
+let mapStateToProps = (state: AppStateType) => {
+    return {
+        isAuth: state.auth.isAuth
+    }
+}
+export function withAuthRedirect <T> (Component: React.ComponentType<T>) {
+    function RedirectComponent(props: MapStateToPropsType) {
+        let {isAuth, ...restProps} = props
+        if (!isAuth) return <Redirect to="/login"/>
+        return <Component {...restProps as T}/>
     }
 
     //коннектим контейнерную компоненту к стору, чтобы не передавать isAuth каждый раз в пропсах
-    let ConnectedRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
+    let ConnectedRedirectComponent = connect(mapStateToProps)(RedirectComponent)
 
     return ConnectedRedirectComponent
 }

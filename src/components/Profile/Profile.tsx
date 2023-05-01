395629d5960f81d0573af001689072b2849fb1b6
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-01 19:39:35 +0300

    в пропсы добавлены status и updateStatus для дальнейшей передачи в ProfileInfo
    (video 73)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import React from 'react';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import {UserProfileType} from '../../redux/profile-reducer';
 
 type ProfilePropsType = {
-    profile: UserProfileType | undefined,
+    profile: UserProfileType | undefined
+    status: string
+    updateStatus: (newStatus: string) => void
 }
 
-function Profile (props: ProfilePropsType) {
+function Profile(props: ProfilePropsType) {
     return (
         <div>
-            <ProfileInfo profile={props.profile}/>
+            <ProfileInfo
+                profile={props.profile}
+                status={props.status}
+                updateStatus={props.updateStatus}
+            />
             <MyPostsContainer/>
         </div>
     )

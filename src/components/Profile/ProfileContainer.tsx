a406f34174ac9c4b1b1bf27e94d6728d255fc84f
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-01 19:34:08 +0300

    добавлена возможность работы со статусом пользователя или со своим
    (video 73)

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Profile from './Profile';
 import {connect} from 'react-redux';
 import {AppStateType} from '../../redux/redux-store';
-import {setUserProfileThunkCreator} from '../../redux/profile-reducer';
+import {
+    getUserStatusThunkCreator,
+    getUserProfileThunkCreator,
+    updateStatusThunkCreator
+} from '../../redux/profile-reducer';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import {withAuthRedirect} from '../../hoc/AuthRedirect';
 import {compose} from 'redux';
@@ -13,52 +17,78 @@ class ProfileClassComponent extends React.Component <RootProfilePropsType> {
     // }
     componentDidUpdate(prevProps: Readonly<RootProfilePropsType>, prevState: Readonly<{}>, snapshot?: any) {
         console.log('ProfileClassComponent.componentDidUpdate')
-          //запрос на сервер выполнится только в том случае, если match.params.userId и profile?.userId будут разные, такое происходит, когда на страничке юзеров кликнули на аватарку, перешли на страничку профиля (с инфой о кликнутом юзере) и при этом кликаем на собственный логин(верхний правый угол), срабатывает componentDidUpdate, в роутинге меняется адрес на profile/наш_айдишник, но в пропсах еще предыдущий юзер сидит со своей айдишкой
-        if (String(this.props.match.params.userId) !== String(this.props.profile?.userId)) {
-            //axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${this.props.match.params.userId}`)
-            this.props.setUserProfile(this.props.match.params.userId)
+        //запрос на сервер выполнится только в том случае, если match.params.userId и profile?.userId будут разные, такое происходит, когда на страничке юзеров кликнули на аватарку, перешли на страничку профиля (с инфой о кликнутом юзере) и при этом кликаем на собственный логин(верхний правый угол) или на profile, срабатывает componentDidUpdate, в роутинге меняется адрес на profile/наш_айдишник или просто на profile/, но в пропсах еще предыдущий юзер сидит со своей айдишкой
+       // debugger
+        //let myId = '28386'
+
+        //тестовый вариант, чтобы при отображении чужого профилотображать мой профиль п
+        //let userId = this.props.match.params.userId
+        let userId = this.props.match.params.userId || '28386'
+        if (userId) {
+            if (String(userId) !== String(this.props.profile?.userId)) {
+                this.props.getUserProfile(userId)
+                this.props.getUserStatus(userId)
+            }
         }
+
+
+        //if (String(this.props.match.params.userId) !== String(this.props.profile?.userId)) {
+        //if (String(this.props.profile?.userId) !== myId) {
+        //    let userId = this.props.match.params.userId || '28386'
+            //this.props.getUserProfile(this.props.match.params.userId)
+            //this.props.getUserStatus(this.props.match.params.userId)
+         //   this.props.getUserProfile(userId)
+        //    this.props.getUserStatus(userId)
+        //}
     }
 
     componentDidMount() {
         //debugger
         console.log('ProfileClassComponent.componentDidMount')
-        let userId = this.props.match.params.userId||'28386'
-        this.props.setUserProfile(userId)
-        //console.log(`userId: ${userId}`)
-        //axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-        //debugger
-        // profileAPI.getProfile(userId).then(data => {this.props.setUserProfile(data)})
+        let userId = this.props.match.params.userId || '28386'
+        this.props.getUserProfile(userId)
+        this.props.getUserStatus(userId)
+
     }
 
     render() {
         //if (!this.props.isAuth) return <Redirect to={'/login'}/>
         return (
-            <Profile profile={this.props.profile}/>
+            <Profile
+                profile={this.props.profile}
+                status={this.props.status}
+                updateStatus={this.props.updateStatus}
+            />
         )
     }
 }
 
 type MapStateToPropsType = ReturnType<typeof mapStateToProps>
 type mapDispatchToPropsType = {
-    setUserProfile: (userId: string) => void;
+    getUserProfile: (userId: string) => void;
+    getUserStatus: (userId: string) => void;
+    updateStatus: (newStatus: string) => void;
 }
 type ProfileConnectPropsType = MapStateToPropsType & mapDispatchToPropsType
 //типизируем параметры, полученные из урл
-type PathParamsType ={
+type PathParamsType = {
     userId: string
 }
-type OwnProfileContainerPropsType = {
-}
+type OwnProfileContainerPropsType = {}
 type RootProfilePropsType = RouteComponentProps<PathParamsType> & ProfileConnectPropsType
 
 
 let mapStateToProps = (state: AppStateType) => ({
     profile: state.profilePage.profile,
+    status: state.profilePage.status,
 })
 
-const ProfileContainer = compose <React.ComponentType>(
-    connect<MapStateToPropsType, mapDispatchToPropsType, OwnProfileContainerPropsType, AppStateType>(mapStateToProps, {setUserProfile: setUserProfileThunkCreator}),
+const ProfileContainer = compose<React.ComponentType>(
+    connect<MapStateToPropsType, mapDispatchToPropsType, OwnProfileContainerPropsType, AppStateType>(mapStateToProps, {
+        getUserProfile: getUserProfileThunkCreator,
+        getUserStatus: getUserStatusThunkCreator,
+        updateStatus: updateStatusThunkCreator,
+    }),
     withRouter,
     withAuthRedirect
 )(ProfileClassComponent)

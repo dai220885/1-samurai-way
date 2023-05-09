2a14a265075a7df1a82ee867819072825f0e662e
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-09 19:55:03 +0300

    addMessageAC теперь принимает текст нового сообщения и добавляет его в экшен. В case ADD_MESSAGE текст сообщения теперь берем из экшена. Также из initialState удалено свойство newMessageText, также удален соответствующий экшенкриэйтор и case в редьюсере. Теперь хранением напечатанного в форме текста до отправки занимется redux-form
    (video 76)

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -2,7 +2,7 @@ import {v1} from 'uuid';
 
 const ADD_MESSAGE = 'ADD-MESSAGE';
 const REMOVE_MESSAGE = 'REMOVE-MESSAGE';
-const SET_NEW_MESSAGE_TEXT = 'SET-NEW-MESSAGE-TEXT';
+//const SET_NEW_MESSAGE_TEXT = 'SET-NEW-MESSAGE-TEXT';
 
 
 export type DialogType = {
@@ -17,7 +17,7 @@ export type MessageType = {
 export type MessagePageType = {
     dialogs: DialogType[]
     messages: MessageType[]
-    newMessageText: string
+    //newMessageText: string
 }
 
 let initialState: MessagePageType = {
@@ -38,14 +38,14 @@ let initialState: MessagePageType = {
         {id: v1(), message: 'Good Buy!!'},
         {id: v1(), message: 'Good Buy!!'},
     ],
-    newMessageText: ''
+    //newMessageText: ''
 }
 
 const dialogsReducer = (state: MessagePageType = initialState, action: DialogsReducerActionType): MessagePageType => {
     switch (action.type) {
         case ADD_MESSAGE:{
-            let newMessage: MessageType = {id: v1(), message: state.newMessageText}
-            return {...state, messages: [newMessage, ...state.messages], newMessageText: ''}
+            let newMessage: MessageType = {id: v1(), message: action.payload.newMessage}
+            return {...state, messages: [newMessage, ...state.messages]}
             // state.messages.push(newMessage)
             // state.newMessageText = ''
             // return state;
@@ -55,11 +55,11 @@ const dialogsReducer = (state: MessagePageType = initialState, action: DialogsRe
             //state.messages = state.messages.filter(m => m.id !== action.payload.messageForRemoveId)
             //return state;
         }
-        case SET_NEW_MESSAGE_TEXT:{
-            return {...state, newMessageText: action.payload.newMessageText}
-            // state.newMessageText = action.payload.newMessageText;
-            // return state;
-        }
+        // case SET_NEW_MESSAGE_TEXT:{
+        //     return {...state, newMessageText: action.payload.newMessageText}
+        //     // state.newMessageText = action.payload.newMessageText;
+        //     // return state;
+        // }
         default:
             return state;
     }
@@ -67,28 +67,24 @@ const dialogsReducer = (state: MessagePageType = initialState, action: DialogsRe
 export type DialogsReducerActionType =
     AddMessageActionType
     | RemoveMessageActionType
-    | SetNewMessageActionType
+    //| SetNewMessageActionType
 //автоматически типизируем ActionCreator-ы, но в ActionCreator-е обязательно добавлять в конце 'as const', чтобы свойство type воспринималось не как любая строка, а как константа:
 export type AddMessageActionType = ReturnType<typeof addMessageAC>
 export type RemoveMessageActionType = ReturnType<typeof removeMessageAC>
-export type SetNewMessageActionType = ReturnType<typeof setNewMessageTextAC>
+//export type SetNewMessageActionType = ReturnType<typeof setNewMessageTextAC>
 
 //функции (ActionCreator-ы), которые будут создавать объекты action (чтобы не запутаться и не ошибиться при создании непосредственно в компоненте
-export const addMessageAC = () => ({type: ADD_MESSAGE}) as const
-export const removeMessageAC = (messageForRemoveId: string) => {
-    return {
-        type: REMOVE_MESSAGE,
-        payload: {
-            messageForRemoveId
-        }
-    } as const
-}
-export const setNewMessageTextAC = (newMessageText: string) => {
-    return {
-        type: SET_NEW_MESSAGE_TEXT,
-            payload: {
-                newMessageText
-            }
-    }as const
-}
+export const addMessageAC = (newMessage: string) =>
+    ({type: ADD_MESSAGE, payload: {newMessage}}) as const
+export const removeMessageAC = (messageForRemoveId: string) =>
+    ({type: REMOVE_MESSAGE, payload: {messageForRemoveId}} as const)
+
+// export const setNewMessageTextAC = (newMessageText: string) => {
+//     return {
+//         type: SET_NEW_MESSAGE_TEXT,
+//             payload: {
+//                 newMessageText
+//             }
+//     }as const
+// }
 export default dialogsReducer;

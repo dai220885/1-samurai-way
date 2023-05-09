a7fa80c093a0c7bed8eba8a70c3884eb05afbdeb
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-09 19:56:25 +0300

    addNewPostAC теперь принимает текст нового поста и добавляет его в экшен. В case ADD_POST текст поста теперь берем из экшена. Также из initialState удалено свойство newPostText, также удален соответствующий экшенкриэйтор и case в редьюсере. Теперь хранением напечатанного в форме текста до отправки занимется redux-form
    (video 76)

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -6,7 +6,7 @@ import {AuthReducerActionType, setAuthUserDataAC} from './auth-reducer';
 
 const ADD_POST = 'ADD-POST';
 const REMOVE_POST = 'REMOVE-POST';
-const SET_NEW_POST_TEXT = 'SET-NEW-POST-TEXT';
+//const SET_NEW_POST_TEXT = 'SET-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
 const SET_STATUS = 'SET-STATUS';
 
@@ -39,7 +39,7 @@ export type UserProfileType = {
 }
 export type ProfilePageType = {
     posts: PostType[]
-    newPostText: string
+    //newPostText: string
     profile: UserProfileType | undefined
     status: string
 }
@@ -50,7 +50,7 @@ let initialState: ProfilePageType = {
         {id: v1(), message: 'By-By', likeCount: 4},
         {id: v1(), message: 'How are you?', likeCount: 1},
     ],
-    newPostText: '',
+    //newPostText: '',
     profile: undefined,
     status: '',
 }
@@ -60,15 +60,15 @@ const profileReducer = (state: ProfilePageType = initialState, action: ProfileRe
     switch (action.type) {
         case ADD_POST: {
             //можно вообще не передавать  извне текст поста, который хотим добавить, а брать его из поля newPostText (которое будет содержать актуальное значение, введенное в TextArea
-            let newPost: PostType = {id: v1(), message: state.newPostText, likeCount: 0}
-            return {...state, posts: [newPost, ...state.posts], newPostText: ''}
+            let newPost: PostType = {id: v1(), message: action.payload.newPost, likeCount: 0}
+            return {...state, posts: [newPost, ...state.posts]}
         }
         case REMOVE_POST: {
             return {...state, posts: state.posts.filter(p => p.id !== action.payload.postForRemoveId)}
         }
-        case SET_NEW_POST_TEXT: {
-            return {...state, newPostText: action.payload.newPostText};
-        }
+        // case SET_NEW_POST_TEXT: {
+        //     return {...state, newPostText: action.payload.newPostText};
+        // }
         case SET_USER_PROFILE: {
             return {...state, profile: action.payload.profile}
         }
@@ -82,21 +82,21 @@ const profileReducer = (state: ProfilePageType = initialState, action: ProfileRe
 export type ProfileReducerActionType =
     AddPostActionType
     | RemovePostActionType
-    | SetNewPostTextActionType
+    //| SetNewPostTextActionType
     |SetUserProfileActionType
     |SetStatusActionType
 
 //автоматически типизируем ActionCreator-ы, но в ActionCreator-е обязательно добавлять в конце 'as const', чтобы свойство type воспринималось не как любая строка, а как константа:
-export type AddPostActionType =  ReturnType<typeof addNewPost>//можно делать так, чтобы не дублировать
-export type RemovePostActionType = ReturnType<typeof removePost> //можно делать так, чтобы не дублировать
-export type SetNewPostTextActionType = ReturnType<typeof setNewPostText>//можно делать так, чтобы не дублировать
+export type AddPostActionType =  ReturnType<typeof addNewPostAC>//можно делать так, чтобы не дублировать
+export type RemovePostActionType = ReturnType<typeof removePostAC> //можно делать так, чтобы не дублировать
+//export type SetNewPostTextActionType = ReturnType<typeof setNewPostText>//можно делать так, чтобы не дублировать
 export type SetUserProfileActionType = ReturnType<typeof setUserProfile>//м
 export type SetStatusActionType = ReturnType<typeof setStatus>//м
 
 //функции (ActionCreator-ы), которые будут создавать объекты action
-export const addNewPost =()=> ({type: ADD_POST}) as const
-export const removePost = (postForRemoveId: string) => ({type: REMOVE_POST, payload: {postForRemoveId}}) as const
-export const setNewPostText = (newPostText:string) => ({type: SET_NEW_POST_TEXT, payload: {newPostText}}) as const
+export const addNewPostAC =(newPost: string)=> ({type: ADD_POST, payload: {newPost}}) as const
+export const removePostAC = (postForRemoveId: string) => ({type: REMOVE_POST, payload: {postForRemoveId}}) as const
+//export const setNewPostText = (newPostText:string) => ({type: SET_NEW_POST_TEXT, payload: {newPostText}}) as const
 export const setUserProfile = (profile: UserProfileType) => ({type: SET_USER_PROFILE, payload: {profile}}) as const //?????fix any type !!!!!!!!!!!!!!!!!!!!!!!!
 export const setStatus = (status: string) => ({type: SET_STATUS, payload: {status}}) as const
 

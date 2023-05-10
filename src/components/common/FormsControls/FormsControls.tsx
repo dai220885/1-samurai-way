c4132b145c01792a4bba2eed543dc770c186ab0a
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-10 21:57:42 +0300

    описаны кастомные InputWithValidate и TextAreaWithValidate (чтобы избавиться от дублирования кода используется FormControl, который возвращает нужный нам элемен, но в таком случае пока не решена проблема типизации)
    (video 77)

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,13 +1,60 @@
-import React, {ChangeEvent, DetailedHTMLProps, KeyboardEvent, TextareaHTMLAttributes} from 'react';
-type TextAreaWithValidatePropsType = {
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent, TextareaHTMLAttributes} from 'react';
+import styles from './FormsControls.module.css'
+type ValidatePropsType = {
     input: {}
-    meta: {}
+    meta: any
     //dispatch?: (action: any) => void
 }
-export const TextAreaWithValidate: React.FC<DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> & TextAreaWithValidatePropsType> = ({input, meta, ...props}) => {
-    //debugger
+
+
+const FormControl: React.FC<any> = ({input, meta, child, ...props}) => {
+
+    const hasError = meta.touched && meta.error
     return (
-        <textarea {...input} {...props}>
-        </textarea>
+        <div className={styles.formControl + ' ' + (hasError? styles.error: '')}>
+            <div>
+                {props.children}
+            </div>
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    );
+}
+
+
+export const TextAreaWithValidate: React.FC<any> = (props) => {
+    const {input, meta, child, ...restProps} = props;
+     return (
+         <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+    );
+};
+
+export const InputWithValidate: React.FC<any> = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return (
+        <FormControl {...props}><input {...input} {...restProps}/></FormControl>
+    );
+};
+
+export const _TextAreaWithValidate: React.FC<DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> & ValidatePropsType> = ({input, meta, ...props}) => {
+    //debugger
+    //console.log(meta)
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={styles.formControl + ' ' + (hasError? styles.error: '')}>
+            <div><textarea {...input} {...props}/></div>
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    );
+};
+
+export const _InputWithValidate: React.FC<DetailedHTMLProps<TextareaHTMLAttributes<HTMLInputElement>, HTMLInputElement> & ValidatePropsType> = ({input, meta, ...props}) => {
+    //debugger
+    //console.log(meta)
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={styles.formControl + ' ' + (hasError? styles.error: '')}>
+            <div><input {...input} {...props}/></div>
+            {hasError && <span>{meta.error}</span>}
+        </div>
     );
 };

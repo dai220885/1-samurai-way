9a4069660cd93a137fe248c8b17f87873f282024
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-16 20:34:48 +0300

    добавлена кнопка вылогинизации
    (video 78)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import logo from '../../logo.svg';
 import classes from './Header.module.css'
 import {NavLink} from 'react-router-dom';
-import {AuthUserDataType} from '../../redux/auth-reducer';
+import {RootHeaderPropsType} from './HeaderContainer';
 
-function Header(props: AuthUserDataType) {
+function Header(props: RootHeaderPropsType) {
     return (
         <header className={classes.header}>
             <div></div>
             <img src={logo} alt="logo"></img>
             <div className={classes.loginBlock}>
                 {props.isAuth
-                    ? <NavLink to={'/profile/'+ props.id}>{props.login}</NavLink>
+                    ? <><NavLink to={'/profile/' + props.id}>{props.login}-</NavLink>
+                        <button onClick={props.logout}>Logout</button>
+                    </>
                     : <NavLink to={'/login'}>Login</NavLink>
                 }
             </div>

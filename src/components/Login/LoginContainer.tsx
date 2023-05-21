48a5b3cff4372592b32040e68405bcd6850fdeec
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-21 21:15:17 +0300

    типизация вынесена вниз кода
    (video 80)

--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-
 import {connect} from 'react-redux';
 import {loginTC} from '../../redux/auth-reducer';
 import {AppStateType} from '../../redux/redux-store';
-
 import Login, {OwnLoginPropsType} from './Login';
 import {compose} from 'redux';
-import {addMessageAC, removeMessageAC} from '../../redux/dialogs-reducer';
-import {withAuthRedirect} from '../../hoc/AuthRedirect';
-import Dialogs from '../Dialogs/Dialogs';
 
-
-export type MapStateToPropsType = ReturnType<typeof mapStateToProps>
 let mapStateToProps = (state: AppStateType) => ({
     isAuth: state.auth.isAuth,
 })
 
+const _LoginContainer = connect <MapStateToPropsType, MapDispatchToPropsType, OwnLoginPropsType, AppStateType> (mapStateToProps, {login: loginTC})(Login)
+
+const LoginContainer = compose  <React.ComponentType> (
+    connect<MapStateToPropsType, MapDispatchToPropsType, OwnLoginPropsType, AppStateType>
+    (mapStateToProps, {login: loginTC}),
+)(Login)
+
+
+//types:
+export type MapStateToPropsType = ReturnType<typeof mapStateToProps>
 export type MapDispatchToPropsType = {
     login: (email:string, password: string, rememberMe: boolean) => void
 }
 
-
-const _LoginContainer = connect <MapStateToPropsType, MapDispatchToPropsType, OwnLoginPropsType, AppStateType> (mapStateToProps, {login: loginTC})(Login)
-
-
-const LoginContainer = compose  <React.ComponentType> (
-    connect<MapStateToPropsType, MapDispatchToPropsType, OwnLoginPropsType, AppStateType>
-    (mapStateToProps, {login: loginTC}
-    ),
-)(Login)
 export default LoginContainer;

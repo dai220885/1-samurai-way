310a1b81acb64336d66359975c3d669250ae2d91
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-22 23:25:44 +0300

    убрал ненужные комментарии
    (video 83)

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store, {AppStateType} from './redux/redux-store';
+import store from './redux/redux-store';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
-
-//import StoreContext, {Provider} from './StoreContext';
-// сначала пользовались провайдером из StoreContext.ts, теперь перешли на Provider из библиотеке 'react-redux'
 import {Provider} from 'react-redux';
-
-//сначала в rerenderEntireTree передавали стейт (вызывали "store.getState()"), чтобы передать его дальше в качестве пропсов, теперь же стейт дальше не передается, поэтому его не нужно передавать в rerenderEntireTree
-
-
-//let rerenderEntireTree = () => {
-//убрали отрисовку из функции, а просто единожды выполнили ее
 //в Provider передаем стор, после чего он будет доступен в контейнерной компоненте через StoreContext.Consumer
 ReactDOM.render(
     <BrowserRouter>
@@ -26,21 +17,6 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('root')
 );
-//}
-//первый вызов функции rerenderEntireTree с передачей в нее актуального стейта для отображения на странице
-//rerenderEntireTree(store.getState());
 
-
-//rerenderEntireTree();
-//store.subscribe(() => rerenderEntireTree(store.getState()))
-
-//больше не нужно субскрайбиться, т.к. "connect", используемый в контейнерных компонентах сделает это за нас
-//store.subscribe(() => rerenderEntireTree())
-
-//далее функцию rerenderEntireTree передаем в качестве параметра в метод store.subscribe() (store.subscribe(rerenderEntireTree)) который  изменит поведение метода 'store._rerenderEntireTree' на поведение функции 'rerenderEntireTree', которая придет в качестве параметра
-
-//store.subscribe(rerenderEntireTree)
-//
-// было так, когда наш store сам передавал актуальный(измененный стейт) в функцию rerenderEntireTree (в методе dispatch), store из реакта так не делает, он просто уведомляет подписчика об изменении стейта, поэтому актуальный стейт нужно передать самостоятельно, для этого в subscribe передаем анонимную функцию, которая запустит наш rerenderEntireTree и передаст в нее актуальный стейт (store.getState())
 
 

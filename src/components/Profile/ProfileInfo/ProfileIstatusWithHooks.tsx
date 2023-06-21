d6ea4133d08af411e1beafcf78a1d88116e6dac8
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-21 22:41:24 +0300

    Описал компонент ProfileStatusWithHooks (функциональная компонента с использованием хука useState)
    (video 84)

---
+++ src/components/Profile/ProfileInfo/ProfileIstatusWithHooks.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type ProfileStatusWithHooksPropsType = {
+	status: string
+	updateStatus: (newStatus: string) => void
+	//profile: UserProfileType | undefined
+}
+
+const ProfileStatusWithHooks = (props: ProfileStatusWithHooksPropsType) => {
+	let [editMode, setEditMode] = useState(false)
+	let [status, setStatus] = useState(props.status)
+	const activateEditMode = () => {
+		setEditMode(true)
+	}
+	const deactivateEditMode = () => {
+		setEditMode(false)
+		props.updateStatus(status)
+	}
+	const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setStatus(e.currentTarget.value)
+	}
+	return (
+		<>
+			{!editMode &&
+          <div>
+              <span onDoubleClick={activateEditMode}>STATUS: {props.status || '____'}</span>
+          </div>
+			}
+			{editMode &&
+          <div>
+              <input
+                  value={status}
+                  onChange={onStatusChange}
+                  onBlur={deactivateEditMode}
+                  autoFocus={true}/>
+          </div>
+			}
+
+		</>
+	)
+}
+
+export default ProfileStatusWithHooks;
